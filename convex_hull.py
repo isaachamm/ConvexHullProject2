636c18f54b1324d9bdeba1028924eec96e76b58e
Isaac Hamm <isaachamm4@gmail.com>
2023-01-31 15:13:59 -0700

    Added comments describing quicksort, and started working on convex hull solver and upper tangent function

--- convex_hull.py
+++ convex_hull.py
@@ -38,7 +38,8 @@ class ConvexHullSolver(QObject):
 # to be displayed.
 
 	def showTangent(self, line, color):
-		self.view.addLines(line,color)
+		lines = [line]
+		self.view.addLines(lines,color)
 		if self.pause:
 			time.sleep(PAUSE)
 
@@ -70,9 +71,12 @@ class ConvexHullSolver(QObject):
 
 		t1 = time.time()
 		# TODO: SORT THE POINTS BY INCREASING X-VALUE
+		# Done by implementing the quicksort and partition algorithm
 
 		def partition(start_index, end_index):
 			# Choose a pivot at random –– constant time
+			# Necessary because it makes quicksort more likely to run average time nlogn, instead of
+			# 	worst case time, n^2
 			pivot_index = random.randint(start_index, end_index)
 
 			# Move the pivot point out of the way (to the start of the array) -- constant time
@@ -82,18 +86,24 @@ class ConvexHullSolver(QObject):
 			low = start_index
 
 			# high is a looper variable that keeps track of the current index
+			# This is where the O(n) work comes in –- comparisons of numbers and merging of two halves
 			for high in range(start_index + 1, end_index + 1):
 				# if math.isclose(points[high].x() < points[pivot_index].x()) :
+				#Don't need the above because we're not performing arithmetic, just comparisons on bounded numbers
 				if points[high].x() < points[start_index].x():
 					low += 1
 					points[low], points[high] = points[high], points[low]
 
+			# Move the pivot back to the new correct position
 			points[low], points[start_index] = points[start_index], points[low]
 
 			return low
 
 		def quicksort(start_index, end_index) :
 
+			# overall, quicksort is O(n log n) time
+			# Quicksort does the logn work as it splits the array in half until it reaches 1 element
+			# the partition function does the n work as it does the comparisons and recombines
 			if start_index < end_index:
 				partition_index = partition(start_index, end_index)
 				quicksort(start_index, partition_index - 1)
@@ -105,9 +115,59 @@ class ConvexHullSolver(QObject):
 		t2 = time.time()
 
 		t3 = time.time()
+
+		def convex_hull_solver(point_array) :
+			if len(point_array) == 0:
+				return
+			elif len(point_array) == 1:
+				# base case = only 1 point left
+				return point_array[0]
+			else:
+				# This is where the divide occurs – logn time
+				left_points = convex_hull_solver(point_array[:(len(point_array) // 2)])	# left-side recurse
+				right_points = convex_hull_solver(point_array[(len(point_array) // 2):]) # right-side recurse
+
+				print(left_points)
+				print(right_points)
+
+				def find_upper_tangent(left_side, right_side):
+					rm_left_point = None
+					lm_right_point = None
+					if type(left_points) is not list:
+						rm_left_point = left_points
+					else:
+						rm_left_point = left_side[:-1]
+
+					if type(right_points) is not list:
+						lm_right_point = right_points
+					else:
+						lm_right_point = right_side[0]
+
+					slope = (rm_left_point.y() - lm_right_point.y()) / (rm_left_point.x() - lm_right_point.x())
+
+
+					# for left_point in left_side:
+					# 	if ()
+
+					return
+
+				find_upper_tangent(left_points, right_points)
+
+				def find_lower_tangent():
+					return
+		# 	while either of the tangent functions change the tangent lines
+		# 		call upper tangent function
+		# 		call lower tangent function
+		# 	return combined hulls, or it won't recurse back up
+
+		convex_hull_solver(points)
+
+
 		# this is a dummy polygon of the first 3 unsorted points
 		polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
 		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
+
+
 		t4 = time.time()
 
 		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
